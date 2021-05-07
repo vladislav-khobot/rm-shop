94d0e12d3126ad1eaf1a0d2ab482bbcdbf2fe251
daminort <daminort@gmail.com>
2021-05-07 18:09:50 +0300

    Add support for .env

---
+++ server/src/config/app.config.ts
@@ -0,0 +1,35 @@
+import * as dotenv from 'dotenv';
+
+interface DatabaseConfig {
+  host: string;
+  user: string;
+  password: string;
+  name: string;
+  connection: string;
+}
+
+interface AppConfig {
+  port: string | number;
+  db: DatabaseConfig;
+}
+
+dotenv.config();
+
+const port = process.env.PORT || 3010;
+
+const host = process.env.DB_HOST;
+const user = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+const name = process.env.DB_NAME;
+const connection = `mongodb+srv://${user}:${password}@${host}/${name}?retryWrites=true&w=majority`
+
+export const appConfig: AppConfig = {
+  port,
+  db: {
+    host,
+    user,
+    password,
+    name,
+    connection,
+  }
+};
