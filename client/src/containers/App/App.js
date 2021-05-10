76a8feb3ca3adbe2929c1e8ff9ef6bc8447f9155
vladislav-khobot <vladislav.khobot@gmail.com>
2021-05-10 17:38:23 +0300

    Added a basic-project-structure

---
+++ client/src/containers/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      thomething for linter
+    </div>
+  );
+}
+
+export default App;
