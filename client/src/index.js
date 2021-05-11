0523df66a77fb7307b7bdae66957d96e0d4a301b
vladislav-khobot <vladislav.khobot@gmail.com>
2021-05-11 15:46:33 +0300

    add index.style.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,26 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { createGlobalStyle } from 'styled-components';
-
 import App from './containers/App/App';
 import reportWebVitals from './reportWebVitals';
 
-import backgroundImage from './assets/images/background-image.png';
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    width: 100vw;
-    height: 100vh;
-    font-size: 16px;
-    font-family: 'Courier New', Courier, monospace;
-    background-image: url(${backgroundImage});
-    background-repeat: no-repeat;
-    background-size: cover;
-  }
-`;
+import { GlobalStyle } from './index.style';
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ client/src/index.style.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+
+import backgroundImage from './assets/images/background-image.png';
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    width: 100vw;
+    height: 100vh;
+    font-size: 16px;
+    font-family: 'Courier New', Courier, monospace;
+    background-image: url(${backgroundImage});
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+`;
