ea37ad5af25e16d82173006324e15f82fe98f92b
daminort <daminort@gmail.com>
2021-05-15 11:25:40 +0300

    Add test for Button

---
+++ client/src/components/Button/Button.test.js
@@ -0,0 +1,54 @@
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import { Button, BUTTON_TYPES } from 'components/Button';
+
+describe('Component Button', () => {
+
+  afterEach(() => {
+    cleanup();
+  });
+
+  const setup = (props = {}) => {
+    return render(
+      <Button {...props} />
+    );
+  };
+
+  test('Normal render', () => {
+    const { getByTestId } = setup();
+
+    const node = getByTestId('button');
+
+    expect(node).toBeInTheDocument();
+  });
+
+  test('Render with type "default"', () => {
+    const { getByTestId } = setup();
+
+    const node = getByTestId('button');
+    const className = node.className;
+
+    expect(node).toBeInTheDocument();
+    expect(className.includes('btn default')).toBeTruthy();
+  });
+
+  test('Render with type "primary"', () => {
+    const { getByTestId } = setup({ type: BUTTON_TYPES.primary });
+
+    const node = getByTestId('button');
+    const className = node.className;
+
+    expect(node).toBeInTheDocument();
+    expect(className.includes('btn primary')).toBeTruthy();
+  });
+
+  test('Event reaction: "onClick"', () => {
+    const onClick = jest.fn();
+
+    const { getByTestId } = setup({ onClick });
+
+    const node = getByTestId('button');
+
+    fireEvent.click(node);
+    expect(onClick).toHaveBeenCalled();
+  });
+});
