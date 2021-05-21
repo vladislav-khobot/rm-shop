92d03871fe1bac864a9bffb32ff4ea4e817d8f98
vladislav-khobot <vladislav.khobot@gmail.com>
2021-05-21 15:05:59 +0300

    implement shirt component

---
+++ client/src/components/Shirt/Shirt.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { THEME } from 'constants/theme';
+
+import { SHIRT_SIZES } from './Shirt.sizes';
+import { StyledShirt } from './Shirt.style';
+
+
+function Shirt(props) {
+  const { size, color, image } = props;
+
+  return (
+    <StyledShirt />
+  );
+}
+
+Shirt.propTypes = {
+  size: PropTypes.oneOf([
+    SHIRT_SIZES.small,
+    SHIRT_SIZES.medium,
+    SHIRT_SIZES.large,
+  ]),
+  color: PropTypes.string,
+  image: PropTypes.string,
+};
+
+Shirt.defaultProps = {
+  size: SHIRT_SIZES.medium,
+  color: THEME.colors.mainBlue,
+  image: '',
+};
+
+export { Shirt };
