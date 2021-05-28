973578bd8f9040aac183d63ce7b10c153ef65018
vladislav-khobot <vladislav.khobot@gmail.com>
2021-05-28 11:03:44 +0300

    change api url

--- client/config.js
+++ client/config.js
@@ -4,10 +4,10 @@ const isProduction = (process.env.NODE_ENV === 'production');
 const envURL = process.env.REACT_APP_API_URL || '';
 const originURL = `${window.location.origin}/api`;
 
-const API_URL = envURL || originURL;
+const apiURL = envURL || originURL;
 
 export {
   isDevelopment,
   isProduction,
-  API_URL,
+  apiURL,
 };
