9c00aa1d30dd222cbfd071864fe78a456c233d9d
vladislav-khobot <vladislav.khobot@gmail.com>
2021-06-01 11:00:21 +0300

    replace components Basket and User

--- client/src/containers/TopBar/TopBar.js
+++ client/src/containers/TopBar/TopBar.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 import { Logo } from 'components/Logo';
-import { Basket } from 'components/Basket';
-import { User } from 'components/User';
+import { Basket } from 'containers/TopBar/Basket';
+import { User } from 'containers/TopBar/User';
 
 import { StyledTopBar } from './TopBar.style';
 
